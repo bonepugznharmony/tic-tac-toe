0c6b11e3c5e07959b276efe31aa1063370293481
ts-oh <tsohlove@gmail.com>
2021-12-15 19:06:56 +0700

    added gameboard module

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-/* 
-1. gameboard is stored as an array inside of Gameboard object
-*/
-
-// create and array with numbers to fill 9 squares
-let boardArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// create a facotry function to make a game board
-function gameBoardFactory(board) {
-  return {
-    board: [
-      [board[0], board[1], board[2]],
-      [board[3], board[4], board[5]],
-      [board[6], board[7], board[8]],
-    ],
+//1. gameboard is stored as an array inside of Gameboard object
+const createGameBoard = (function () {
+  const board = function () {
+    let boardArray = {
+      grid: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ],
+    }
+    return boardArray
   }
-}
+  return { board }
+})()
 
-// call factory function to creat gameboard object
-const newBoard = gameBoardFactory(boardArray)
+const newBoard = createGameBoard.board()
 console.log(newBoard)
 
-/*
-2. create an object that creates players
+// call factory function to create gameboard object
+// 2. create an object that creates players
+// factory function
 
+/*
 3. an object that controls the flow of the game
 
 4. if you only ever need ONE of something (gameBoard, displayController)

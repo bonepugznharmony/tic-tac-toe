8d9813174b928cb6b19efc960b75801cea57330c
ts-oh <tsohlove@gmail.com>
2021-12-23 14:43:27 +0700

    function to start game flow logic

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-//1. Store the gameboard array inside of a gameboard object
-
+// 1. Create a IFFE module to create a gameboard object
 const createGameBoard = (function () {
   const boardArray = function () {
     return {
-      grid: [
-        ['1', '2', '3'],
-        ['4', '5', '6'],
-        ['7', '8', '9'],
-      ],
+      grid: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
     }
   }
-
   const printBoard = function () {
     return boardArray()
   }
   return { printBoard }
 })()
 
-let newBoard = createGameBoard.printBoard()
-console.log(newBoard) // => { grid: [ [ '1', '2', '3' ], [ '4', '5', '6' ], [
-// '7', '8', '9' ] ] }
+let gameboard = createGameBoard.printBoard()
 
-// 2. create a factory function that creates players objects
-
+// 2. Create a factory function that creates two player objects
 const playerFactory = function (name, mark) {
   const setName = name
   const setMark = mark
-
   const getName = function () {
     return setName
   }
-
   const getMark = function () {
     return setMark
   }
-
   return { getName, getMark }
 }
 
 const player1 = playerFactory('Player 1', 'X')
-console.log(player1)
 const player2 = playerFactory('Player 2', 'O')
-console.log(player2)
 
-// 3. an object that controls the flow of the game
+// 3. Function to start game logic
+// function takes three parameters player1, player2 and gameboard.
+function playGame(player1, player2, gameboard) {
+  // player turn
+  let playerTurns
 
-// 4. if you only ever need ONE of something (gameBoard, displayController)
-
-// 5. If you need multiples of something (players!), create them with factories
+  // if player turn is player1
+  if (playerTurn === player1) {
+    // put player1 mark down on the gameboard
+    // check winning condition (combo)
+    // if win is detected announce winner
+    // otherwise winning condition is false
+    // switch to player2
+  } else if (playerTurn === player2) {
+    // put player2 mark down on the gameboard
+    // check winning condition (combo)
+    // if win is detected announce winner
+    // otherwise winning condition is false
+    // switch to player 1
+  }
+}

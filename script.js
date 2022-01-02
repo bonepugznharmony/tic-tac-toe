cca411f4ec1e2ef90e1725998e2cf121b269b4cf
ts-oh <tsohlove@gmail.com>
2022-01-02 21:14:53 +0700

    change to arrow function, remove parameters from game() and modify player switch

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const gridDisplayArea = document.querySelector('.testing-space')
 
 // 1. Create a IFFE module to create a gameboard object
-const createGameBoard = (function () {
-  const boardArray = function () {
+const createGameBoard = (() => {
+  const boardArray = () => {
     return {
       grid: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
     }
   }
-  const printBoard = function () {
+  const printBoard = () => {
     return boardArray()
   }
   return { printBoard }
@@ -16,13 +16,13 @@ const createGameBoard = (function () {
 let gameboard = createGameBoard.printBoard()
 
 // 2. Create a factory function that creates two player objects
-const playerFactory = function (name, mark) {
+const playerFactory = (name, mark) => {
   const setName = name
   const setMark = mark
-  const getName = function () {
+  const getName = () => {
     return setName
   }
-  const getMark = function () {
+  const getMark = () => {
     return setMark
   }
   return { getName, getMark }
@@ -32,19 +32,22 @@ const player1 = playerFactory('Player 1', 'X')
 const player2 = playerFactory('Player 2', 'O')
 
 // 3. Function to start game logic
-// function takes three parameters player1, player2 and gameboard.
-function gameFlow(player1, player2, gameboard) {
-  // if player turn is player1
-  let currentPlayer = player1
+function gameFlow() {
+  let player1Turn = true
   // player switch function
-  const switchPlayers = function () {
-    if (currentPlayer === player1) {
-      currentPlayer = player2
+  // if player turn is player1
+  const switchPlayers = () => {
+    if (player1Turn === true) {
+      // place mark for player1 function
     } else {
-      currentPlayer = player1
+      // place mark for player 2
     }
+    //switch player
+    //is player 1 turn not true?
+    player1Turn = !player1Turn
   }
 
+  const playerMark = () => {
   //place mark on the index 0
   console.log(currentPlayer)
   gameboard.grid[0] = player1.getMark()
@@ -77,5 +80,4 @@ const displayController = (function () {
   return { displayBoard }
 })()
 
-// Once project is finished, store all invocation flow to main()
-gameFlow(player1, player2, gameboard)
+// Once project is finished, store all invocation flow to main()?
